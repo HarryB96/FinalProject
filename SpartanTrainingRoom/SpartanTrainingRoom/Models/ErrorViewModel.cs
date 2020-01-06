22adfd712c15e6076f8cdda0e43779f740ce0d33
HarryB96 <57000678+HarryB96@users.noreply.github.com>
2020-01-06 12:07:53 +0000

    Created ASP.Net core project

---
+++ SpartanTrainingRoom/SpartanTrainingRoom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpartanTrainingRoom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
