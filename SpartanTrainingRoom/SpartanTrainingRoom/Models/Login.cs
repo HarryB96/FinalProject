dcc440532ac2867582483b40458b128f49f4f015
HarryB96 <57000678+HarryB96@users.noreply.github.com>
2020-01-07 18:00:58 +0000

    Attempting to create validation for a login

---
+++ SpartanTrainingRoom/SpartanTrainingRoom/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpartanTrainingRoom.Models
+{    
+    public class Login
+    {
+        TrainingDbModel model;
+        public void ValidateUser(string email)
+        {
+            model.Users.Where<User>(u => u.Email == email);
+        }
+    }
+}
