1c2f603c17f42377e683db29775462f6d4555649
HarryB96 <57000678+HarryB96@users.noreply.github.com>
2020-01-07 12:05:19 +0000

    Added models and controllers to the project

---
+++ SpartanTrainingRoom/SpartanTrainingRoom/Models/TrainingDbModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpartanTrainingRoom.Models
+{
+    public class TrainingDbModel : DbContext
+    {
+        public TrainingDbModel(DbContextOptions<TrainingDbModel> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<TypeOfUser> TypeOfUser { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Course> Course { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<TypeOfUser>().Property(t => t.TypeName).IsRequired();
+        //    modelBuilder.Entity<Course>().Property(c => c.CourseName).IsRequired();
+        //}
+    }
+}
---
+++ SpartanTrainingRoom/SpartanTrainingRoom/Models/TypeOfUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpartanTrainingRoom.Models
+{
+    public class TypeOfUser
+    {
+        [Key]
+        public int TypeId { get; set; }
+        [StringLength(20)]
+        public string TypeName { get; set; }
+        public bool CanEditUsers { get; set; }
+        public bool CanAddAdmins { get; set; }
+    }
+}
