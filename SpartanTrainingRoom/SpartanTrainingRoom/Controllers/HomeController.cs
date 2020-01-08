067850fceb58b757fad0becbfde0270b7e9e88cb
Nathan ingleton <nathan.ingleton@googlemail.com>
2020-01-08 12:09:53 +0000

    created questions view, and added option on navbar

--- SpartanTrainingRoom/SpartanTrainingRoom/Controllers/HomeController.cs
+++ SpartanTrainingRoom/SpartanTrainingRoom/Controllers/HomeController.cs
@@ -29,15 +29,25 @@ namespace SpartanTrainingRoom.Controllers
             return View();
         }
 
+        public IActionResult Questions()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
